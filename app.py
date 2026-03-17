57d5741a970b5b065de2ac10dc9f524ef86aeea6
Abdallaelnhrawy <abdallaelnhrawy43@gmail.com>
2026-03-17 21:54:49 +0200

    fix gunicorn conflict

--- app.py
+++ app.py
@@ -209,4 +209,5 @@ def api_data():
     with open("users.json") as f:
         return json.load(f)
 
-app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
